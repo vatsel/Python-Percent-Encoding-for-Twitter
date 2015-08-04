898a722e785c13ef49af5f7c9cfe272e4d8c6236
vatsel <vatselart@gmail.com>
2015-08-04 15:22:36 +0100

    better comments

--- PercentEncode/__init__.py
+++ PercentEncode/__init__.py
@@ -1,6 +1,6 @@
 def forTwitter(string):
-    '''percent encodes everything except alphanumerics'''
-    twitterAllowedChars = set([48,49,50,51,52,53,54,55,56,57, #numbers
+    '''percent encodes everything except alphanumerics and 4 twitter specific chars'''
+    twitterAllowedChars = set([48,49,50,51,52,53,54,55,56,57, #numbers decimal encoding
                           65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90, #uppercase
                           97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122, #lowercase
                           45,46,95,125 #dash,dot,underscore,tilde                         
@@ -12,7 +12,7 @@ def forTwitter(string):
         if ordinal in twitterAllowedChars: encoded += char
         else:
             hexcode = hex(ord(char))
-            if '0x' != hexcode[:2]: raise ValueError ('python hex() generated weird output ='+str(hexcode))
+            if '0x' != hexcode[:2]: raise ValueError ('python hex() generated a weird output ='+str(hexcode))
             encoded += '%'+hexcode[2:]
     return encoded
         
