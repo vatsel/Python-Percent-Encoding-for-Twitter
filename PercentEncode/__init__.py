c73a370f2a1edc7bc5c050a5d69c6e7ac831cb6a
Mark Vatsel <mark@vatsel.com>
2016-06-22 15:29:35 +0100

    improved encoding

--- PercentEncode/__init__.py
+++ PercentEncode/__init__.py
@@ -1,15 +1,16 @@
+from string                     import ascii_letters, digits
+
 def forTwitter(string):
-    '''percent encodes everything except alphanumerics and 4 twitter specific chars'''
-    twitterAllowedChars = set([48,49,50,51,52,53,54,55,56,57, #numbers decimal encoding
-                          65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90, #uppercase
-                          97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122, #lowercase
-                          45,46,95,125 #dash,dot,underscore,tilde                         
-                          ])
-    if type(string) is not str: raise TypeError("string expected, received type "+str(type(string)))
+    TWITTER_ALLOWED_CHARS = frozenset(ascii_letters + digits + '-._~')
+    if type(string) is not str:  
+        string = str(string)
     encoded = ''
     for char in string:
-        ordinal = ord(char)
-        if ordinal in twitterAllowedChars: encoded += char
-        else: encoded += '%'+hex(ordinal)[2:] #replace 0x prefix with %
+        if char in TWITTER_ALLOWED_CHARS: 
+            encoded += char
+        else:
+            # percent encode every byte in the unicode char 
+            unicode_char = char.encode()
+            for byte in unicode_char:
+                encoded += ''.join(['%' + str(hex(int(byte)))[2:].upper()]) 
     return encoded
-        
